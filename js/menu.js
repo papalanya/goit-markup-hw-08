3258e449644f784dc3ad3c751e7a73771fe2b7b1
Illya Pavlenko <illiampavlenko@gmail.com>
2022-07-30 03:09:33 +0300

    add mobile-menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,16 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const body = document.querySelector("[data-body]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("menu__button--is-open");
+    menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle("menu__button--is-open");
+    mobileMenuRef.classList.toggle("is-open");
+    body.classList.toggle("no-scroll");
   });
 })();
